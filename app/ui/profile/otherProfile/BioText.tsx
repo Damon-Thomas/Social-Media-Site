d83b0efa3e48da508ea48b28ad363a79790d50e2
Damon Thomas <damon22t@gmail.com>
2025-04-29 12:06:53 -0400

    feat: enhance user profile display; add user join date, follower and friend counts, and structure for latest activity, posts, comments, and likes

--- app/ui/profile/otherProfile/BioText.tsx
+++ app/ui/profile/otherProfile/BioText.tsx
@@ -2,13 +2,16 @@ import { ReactNode } from "react";
 
 export default function BioText({ children }: { children: ReactNode }) {
   if (!children) {
-    return <p className="">No bio available</p>;
+    return (
+      <div className="">
+        <p className="">No bio available</p>
+      </div>
+    );
   }
 
   return (
-    <div className="mt-2">
-      <h2 className="text-lg font-semibold">Bio</h2>
-      <p className="mt-1 ">{children}</p>
+    <div className="">
+      <p className="">{children}</p>
     </div>
   );
 }
--- app/ui/profile/otherProfile/PersInfo.tsx
+++ app/ui/profile/otherProfile/PersInfo.tsx
@@ -20,7 +20,36 @@ export default function PersInfo({ userData }: { userData: User }) {
       <div className="flex grow">
         <div className="grow flex flex-col gap-2">
           <h1 className="text-2xl font-bold">{userData.name}</h1>
+          <p>
+            {`Joined on: 
+            ${userData.createdAt.toLocaleDateString("en-US", {
+              month: "short",
+              day: "numeric",
+              year: "numeric",
+            })}`}
+          </p>
           <BioText>{userData.bio}</BioText>
+          <div className="flex gap-4">
+            <p className="whitespace-nowrap">
+              {userData.followers?.length} followers
+            </p>
+            <p className="whitespace-nowrap">
+              {" "}
+              {userData.friends?.length} friends
+            </p>
+          </div>
+          <div className="h-20 bg-slate-500 w-full">
+            <h2>Latest Activity</h2>
+          </div>
+          <div className="h-20 bg-amber-300 w-full">
+            <h2>Posts</h2>
+          </div>
+          <div className="h-20 bg-cyan-700 w-full">
+            <h2>Comments</h2>
+          </div>
+          <div className="h-20 bg-amber-800 w-full">
+            <h2>Likes</h2>
+          </div>
         </div>
         <CardWrapper className="flex flex-col gap-2">
           <h3 className="font-bold md:text-lg">Connections</h3>

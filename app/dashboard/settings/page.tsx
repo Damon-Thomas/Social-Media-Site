ba160c96d46bbfbe9462d320a397b6152682ef77
Damon Thomas <damon22t@gmail.com>
2025-05-06 11:23:55 -0400

    feat: implement Modal core component

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -1,8 +1,32 @@
+"use client";
+
+import Modal from "@/app/ui/core/Modal";
+import { useState } from "react";
+
 export default function SettingsPage() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
       <h1 className="text-2xl font-bold">Settings</h1>
       <p className="text-lg">This is the settings page.</p>
+      <Modal hidden={isModalOpen}>
+        <div className=" flex flex-col items-center justify-center w-full h-full">
+          <h1 className="text-2xl font-bold">Modal</h1>
+          <p className="text-lg">Testing the modal.</p>
+          <button
+            className="bg-[var(--rdmono)] py-2 px-4 rounded text-[var(--dmono)] mt-4"
+            onClick={() => setIsModalOpen(!isModalOpen)}
+          >
+            Toggle Modal
+          </button>
+        </div>
+      </Modal>
+      <button
+        className="bg-[var(--dmono)] py-2 px-4 rounded text-[var(--rdmono)] mt-4"
+        onClick={() => setIsModalOpen(!isModalOpen)}
+      >
+        Toggle Modal
+      </button>
     </div>
   );
 }
---
+++ app/ui/core/Modal.tsx
@@ -0,0 +1,46 @@
+import { ReactNode } from "react";
+
+export default function Modal({
+  children,
+  hidden = true,
+  setHidden,
+}: {
+  children: ReactNode;
+  hidden: boolean;
+  setHidden?: () => void;
+}) {
+  return (
+    <div
+      className={`fixed inset-0 flex items-center justify-center z-50 transition-opacity duration-300 ${
+        hidden ? "opacity-0 pointer-events-none" : "opacity-100"
+      }`}
+      role="dialog"
+      aria-hidden={hidden}
+      aria-labelledby="modal-title"
+      aria-describedby="modal-description"
+    >
+      {/* Background overlay */}
+      <div
+        className="absolute inset-0 bg-[var(--rdmono-90)]  transition-opacity duration-300"
+        onClick={setHidden} // Close modal when clicking outside
+      />
+
+      {/* Modal content */}
+      <div className="relative bg-[var(--dmono)] text-[var(--rdmono)] rounded-lg shadow-lg p-6 z-10 w-full max-w-lg">
+        {/* Close button */}
+        {setHidden && (
+          <button
+            onClick={setHidden}
+            className="absolute top-3 right-3 text-[var(--rdmono)] bg-[var(--dmono)] bg-opacity-50 rounded-full p-2 hover:bg-opacity-75"
+            aria-label="Close modal"
+          >
+            ✕
+          </button>
+        )}
+
+        {/* Modal content */}
+        {children}
+      </div>
+    </div>
+  );
+}

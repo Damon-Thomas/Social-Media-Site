70ae75aedc926dd574012991bb6053335cd85291
Damon Thomas <damon22t@gmail.com>
2025-05-17 13:15:16 -0400

    add theme selection to settings page and update user model for theme preference

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -27,6 +27,24 @@ export default function SettingsPage() {
       >
         Toggle Modal
       </button>
+      <div className="themeButtons flex gap-0">
+        <button
+          className="bg-[var(--dmono)] py-2 px-4 rounded text-[var(--rdmono)] mt-4"
+          onClick={() => {
+            document.documentElement.setAttribute("data-theme", "light");
+          }}
+        >
+          Light
+        </button>
+        <button
+          className="bg-[var(--dmono)] py-2 px-4 rounded text-[var(--rdmono)] mt-4"
+          onClick={() => {
+            document.documentElement.setAttribute("data-theme", "dark");
+          }}
+        >
+          Dark
+        </button>
+      </div>
     </div>
   );
 }

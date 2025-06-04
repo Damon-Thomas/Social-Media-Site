f4e407df259480c45c21b365aa1c4f3e7c5ae975
Damon Thomas <damon22t@gmail.com>
2025-06-04 11:32:48 -0400

    theme switch style tweaks

--- app/ui/settings/ThemeSwitch.tsx
+++ app/ui/settings/ThemeSwitch.tsx
@@ -36,12 +36,16 @@ export default function ThemeSwitch() {
 
   return (
     <button
-      className={`w-20 rounded-l-3xl relative rounded-r-3xl bg-[var(--dmono-30)] text-[var(--rdmono)] h-11 rounded outline-2 outline-[var(--dmono)] no-scale overflow-hidden`}
+      className={`w-20 rounded-l-3xl relative rounded-r-3xl bg-[var(--dmono-50)] text-[var(--rdmono)] h-11 rounded outline-2 ${
+        theme === "dark"
+          ? "outline-[var(--dmono-90)]"
+          : "outline-[var(--darkgrey)]"
+      } no-scale overflow-hidden`}
       onClick={handleThemeChange}
     >
       <div
         className={`h-10 w-10 absolute top-0.5 transition-all duration-300 ease ${
-          theme === "dark" ? "left-0.5" : "left-[calc(100%-2.6rem)]"
+          theme === "dark" ? "left-[0.1rem]" : "left-[calc(100%-2.6rem)]"
         } bg-[var(--aBlack)] rounded-full shadow-md hover:shadow-lg z-50 ${
           isSpinning ? "spin-animation" : ""
         } flex items-center justify-center 

ce24076e10563adc5cabc3ad85691b3f35e0568e
Damon Thomas <damon22t@gmail.com>
2025-06-08 09:40:57 -0400

    feat: update favicon paths and add new favicon images

--- app/head.tsx
+++ app/head.tsx
@@ -6,24 +6,21 @@ export default function Head() {
         name="viewport"
         content="width=device-width, initial-scale=1 , viewport-fit=cover"
       />
-      <link rel="icon" href="/favicon_io%202/favicon-32x32.png" sizes="32x32" />
-      <link rel="icon" href="/favicon_io%202/favicon-16x16.png" sizes="16x16" />
-      <link
-        rel="apple-touch-icon"
-        href="/favicon_io%202/apple-touch-icon.png"
-      />
+      <link rel="icon" href="/favicon/favicon-32x32.png" sizes="32x32" />
+      <link rel="icon" href="/favicon/favicon-16x16.png" sizes="16x16" />
+      <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
       <link
         rel="icon"
-        href="/favicon_io%202/android-chrome-192x192.png"
+        href="/favicon/android-chrome-192x192.png"
         sizes="192x192"
       />
       <link
         rel="icon"
-        href="/favicon_io%202/android-chrome-512x512.png"
+        href="/favicon/android-chrome-512x512.png"
         sizes="512x512"
       />
-      <link rel="manifest" href="/favicon_io%202/site.webmanifest" />
-      <link rel="shortcut icon" href="/favicon_io%202/favicon.ico" />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <link rel="shortcut icon" href="/favicon/favicon.ico" />
       {/* Add any additional head tags as needed */}
     </>
   );
